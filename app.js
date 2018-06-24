d9b8a810cdf22c478b2e95ae2e5516856e0f4405
KENDLS <kathistolzer@hotmail.com>
2018-06-24 20:16:46 +0200

    #002 Add animation

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-$(document).ready(function () {
-    $('label').hover(function(){
-        $(this).prev('.ontent__image').addClass('onhover');
-    }, function(){
-        $(this).prev('.ontent__image').removeClass('onhover');
-    });
-});
+    // Get popUp element
+    var popUp = document.getElementById('simpleModal');
+   // Get open popUp element  
+    var modalBtn = document.getElementById('modalBtn');
+    // Get close popUp element  
+    var close = document.getElementsByClassName('close')[0];
+    
+    // Listen for open click
+    modalBtn.addEventListener('click', openModul);
+    // Listen for close click
+    close.addEventListener('click', closeModul);
+    // Listen for outside click
+    window.addEventListener('click', clickOutside);
+    
+    function openModul() {
+        popUp.style.display = "block";
+    }
+    function closeModul() {
+        popUp.style.display = "none";
+    }
+    function clickOutside(e) {
+        if(e.target == popUp) {
+            popUp.style.display = 'none';
+        }
+    }
 
-$(document).ready(function(){
-    $('label').hover(function(){
-        $(this).prev('.content__image').addClass('onhover');
-    }, function(){
-        $(this).prev('.content__image').removeClass('onhover');
-    });
-});
+
